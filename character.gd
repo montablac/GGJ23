b64871f37943ba0d3af1eda301f1d8b4af3a4038
Joshua P <65388968+montablac@users.noreply.github.com>
2023-02-04 11:39:59 +0800

    Update character.gd

--- character.gd
+++ character.gd
@@ -4,10 +4,11 @@ extends KinematicBody
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var Speed = 5.0;
+var Speed = 5;
 var Force = Vector3(0.0,0.0,0.0);
 var JumpDirection = Vector3(0.0,0.0,0.0)
 var JumpAmount = 0
+var SavedDirection = 0
 var Jumping = false
 
 # Called when the node enters the scene tree for the first time.
@@ -15,29 +16,29 @@ func _ready():
 	pass # Replace with function body.
 
 func _physics_process(delta):
-	var Gravity = 5.0
+	var Gravity = 1.0
 	
-	Force[1] = -Gravity
+	Force[1] -= Gravity
 	Force = self.move_and_slide(Force,Vector3(0.0,1.0,0.0),true);
+	print(Force)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
 	var Direction = 0;
-	var JumpPower = 100;
+	var JumpPower = 20;
 	var NotMoving = true
 	var Gravity = 5.0
 	
 	Force = Vector3(0.0,Speed * -1.0,0.0);
 	
 	if(self.is_on_floor()):
-		Force[0] = 0.0
 		Jumping = false
 	
-	if(Input.is_key_pressed(KEY_D)):
+	if(Input.is_key_pressed(KEY_D) && !Jumping):
 		Direction += 1.0;
 	
-	if(Input.is_key_pressed(KEY_A)):
+	if(Input.is_key_pressed(KEY_A) && !Jumping):
 		Direction += -1.0;
 	
 	if(Input.is_key_pressed(KEY_SPACE)):
@@ -45,13 +46,13 @@ func _process(delta):
 			JumpDirection = self.get_floor_normal()
 			Jumping = true
 			JumpAmount = JumpPower
-	
-	
-	Force[0] = Direction * Speed  
-		
+			SavedDirection = Direction
 	
 	if(Jumping):
+		Force[0] += SavedDirection * Speed 
 		JumpAmount -= JumpPower * delta
-		if(JumpAmount <= JumpPower):
+		if(JumpAmount >= 0):
 			Force += JumpDirection * JumpAmount
+	else:
+		Force[0] += Direction * Speed
 	
