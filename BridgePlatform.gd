cc9e1fd53dd09c039d8c7a3d055523110477cef3
Joshua P <65388968+montablac@users.noreply.github.com>
2023-02-05 13:21:28 +0800

    update

---
+++ BridgePlatform.gd
@@ -0,0 +1,21 @@
+extends StaticBody
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var Player = get_tree().get_root().find_node("Player", true, false)
+	Player.connect("Change_World",self,"Vanish")
+
+func Vanish(passes):
+	if(passes == 2):
+		$CollisionShape["disabled"] = true
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
---
+++ Change.gd
@@ -0,0 +1,23 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var Player = get_tree().get_root().find_node("Player", true, false)
+	Player.connect("Change_World",self,"Vanish")
+
+func Vanish(passes):
+	if(passes == 2):
+		$"."["visible"] = false
+		#$Area/["monitoring"] = false
+		#$Area/["monitorable"] = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
